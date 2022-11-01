05ad23d8683685bb0987f8071535aa2857a16635
Saw Nerdar <sheep.blinker@gmail.com>
2022-11-01 16:52:03 +0630

    fixed curl

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -3,10 +3,9 @@
 namespace App\Http\Controllers;
 
 use CURLFile;
-use GuzzleHttp\Client;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-
 class FacebookController extends Controller {
 
     public function profile($page_id) {
@@ -45,67 +44,40 @@ class FacebookController extends Controller {
         ], 200);
     }
 
-    public function sendVoice(Request $req) {
-        // $filedata = $req->file('filedata');
-        $filedata = $req->filedata;
-        // $filedata = fopen($filedata, 'r');
-        $recipient_id = "$req->recipient_id";
-        $access_token = $req->access_token;
-
-        //  $req->file('filedata');
-        // $ext = $filedata->getClientOriginalExtension();
-        // $ext = 'wav';
-        // $fileQuery = '@' . $filedata . '.' . $ext . ';type=audio/' . $ext;
-        // $fileQuery = $filedata;
-        // $fileQuery = file_get_contents($filedata);
-
-        $url = 'https://graph.facebook.com/v15.0/me/messages';
-        // $queryString = "?recipient={id:" . $req->recipient_id . "}&message={'attachment':{'type':'audio'}}&filedata=" . $fileQuery . "&access_token=" . $req->access_token;
-
-        // $response = Http::withHeaders([
-        //     'Content-Type' => 'multipart/form-data'
-        // ])->post(
-        //     $url . $queryString
-        //     // "https://graph.facebook.com/v15.0/me/messages?recipient={id:5258000234291901}&message={'attachment':{'type':'audio'}}&filedata=$req->filedata&access_token=$req->access_token"
-        // );
+    public function sendVoice(Request $request) {
         try {
-            // $f = file_get_contents($filedata);
-
-            // $response = Http::attach('recipient', $recipient_id)
-            //     ->attach('message', $message)
-            //     ->attach('filedata', $filedata)
-            //     ->withHeaders([
-            //         'Content-Type' => 'multipart/form-data',
-            //     ])->post($url . '?access_token=' . $access_token);
-
-            $url = "https://graph.facebook.com/v15.0/100614572669575/messages?recipient={id:$recipient_id}&message={'attachment':{'type':'audio'}}&filedata='@$filedata.wav'&access_token=$access_token";
-
-            // $response = Http::attach('filedata', $filedata)
-            //     ->withHeaders([
-            //         'Content-Type' => 'audio/wav',
-            //     ])->post("https://graph.facebook.com/v15.0/100614572669575/messages?recipient={id:$recipient_id}&message={'attachment':{'type':'audio'}}&filedata='@$filedata.wav'&access_token=$access_token");
-
             $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/v15.0/me/messages?access_token=$access_token");
+            curl_setopt($ch, CURLOPT_URL, "https://graph.facebook.com/v15.0/me/messages?access_token=$request->access_token");
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, [ 'Content-Type' => 'multipart/form-data']);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
             curl_setopt($ch, CURLOPT_POSTFIELDS, [
-                'recipient' => "{'id':'$recipient_id'}",
-                'message' => '{"attachment":{"type":"audio", "payload":{"is_reusable":true}}}',
-                'filedata' => new CURLFile($filedata),
+                'recipient' => $request->recipient,
+                'message' => json_encode([
+                    'attachment' => [
+                        'type' => 'audio',
+                        'payload' => [ 'is_reusable' => false ]
+                    ]
+                ]),
+                'filedata' => new CURLFile($request->file('filedata'), 'audio/webm')
             ]);
-
             $response = curl_exec($ch);
 
-            return response()->json([
-                'api response json' => $response,
-                'url' => $url
-            ], 200);
-        } catch (\Exception $e) {
-            return response()->json([
-                'exception' => $e,
-                'api response json' => $response,
-            ], 500);
+            $this->senderAction('mark_seen');
+
+            if (curl_errno($ch)) {
+                return response(curl_error($ch));
+            }
+            curl_close($ch);
+
+            return response()->json($response, 200);
         }
+        catch(Exception $e) {
+            return response($e, 500);
+        }
+    }
+
+    public function senderAction($action) {
+
     }
 }
