ee16867b198bac6602bc41d4843685e5427eed42
Jupiter <jupitersatt001@protonmailcom>
2022-11-09 12:19:51 +0630

    notification log first try

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -11,55 +11,56 @@ use App\Models\UserPage;
 use Illuminate\Http\Request;
 use Exception;
 use Illuminate\Support\Facades\File;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class MessageController extends Controller {
-  protected $message, $settings;
+    protected $message, $settings;
 
-  public function __construct(
-    MessageRepositoryInterface $message,
-    SettingRepositoryInterface $settings
-  ) {
-    $this->message = $message;
-    $this->settings = $settings;
-    $this->token = env('FACEBOOK_VERIFICATION_TOKEN', 'ChatTesting');
-  }
-
-  public function browse($fb_page_id) {
-    try {
-      $result = $this->message->browse(auth()->user()->id, (int) $fb_page_id);
-
-      return $this->successResponse(
-        $result,
-        200
-      );
-    } catch (Exception $e) {
-      $this->er500($e->getMessage());
+    public function __construct(
+        MessageRepositoryInterface $message,
+        SettingRepositoryInterface $settings
+    ) {
+        $this->message = $message;
+        $this->settings = $settings;
+        $this->token = env('FACEBOOK_VERIFICATION_TOKEN', 'ChatTesting');
     }
-  }
 
-  public function add($page_id, Request $request) {
-    try {
+    public function browse($fb_page_id) {
+        try {
+            $result = $this->message->browse(auth()->user()->id, (int) $fb_page_id);
 
-      $request->message_type_id = auth()->user()->user_role_id === 3 ? 1 : 2;
-      $request->user_id = auth()->user()->id;
-      $request->fb_page_id = $page_id;
+            return $this->successResponse(
+                $result,
+                200
+            );
+        } catch (Exception $e) {
+            $this->er500($e->getMessage());
+        }
+    }
 
-      /*
+    public function add($page_id, Request $request) {
+        try {
+
+            $request->message_type_id = auth()->user()->user_role_id === 3 ? 1 : 2;
+            $request->user_id = auth()->user()->id;
+            $request->fb_page_id = $page_id;
+
+            /*
       $data = $request->only([
         'message',
       ]);
       */
 
-      $data['message'] = $request->message;
-      $data['message_type_id'] = auth()->user()->user_role_id === 3 ? 1 : 2;
-      $data['user_id'] = auth()->user()->id;
-      $data['fb_page_id'] = (int) $page_id;
+            $data['message'] = $request->message;
+            $data['message_type_id'] = auth()->user()->user_role_id === 3 ? 1 : 2;
+            $data['user_id'] = auth()->user()->id;
+            $data['fb_page_id'] = (int) $page_id;
 
-      $msg = $this->message->save(null, $data);
+            $msg = $this->message->save(null, $data);
 
-      return response()->json($msg, 201);
+            return response()->json($msg, 201);
 
-      /*
+            /*
       return $this->successResponse(
         $this->message->save(
           null,
@@ -74,86 +75,109 @@ class MessageController extends Controller {
       );
       */
 
-      return response()->json(['hi']);
+            return response()->json(['hi']);
 
 
-      // |*****************************|
-      // |TODO: Fire message sent event|
-      // |*****************************|
+            // |*****************************|
+            // |TODO: Fire message sent event|
+            // |*****************************|
 
 
-    } catch(Exception $e) {
-      return $this->er500($e->getMessage());
+        } catch (Exception $e) {
+            return $this->er500($e->getMessage());
+        }
     }
-  }
 
-  public function receiveNotification(Request $request) {
-    // $firebaseToken = $this->settings->viewByName('firebase_token');
-    $log = new FacebookNotificationLog;
+    public function receiveNotification(Request $request) {
+        // $firebaseToken = $this->settings->viewByName('firebase_token');
+        $log = new FacebookNotificationLog;
 
-    try {
-      // saving to log
-      $page = FbPage::where('page_id', '=', $request->entry[0]['id'])->first();
-      $userPage = UserPage::where('page_id', '=', $page->id)->get();
-      $users = User::whereIn('id', $userPage->pluck('user_id'))->get();
+        try {
+            // saving to log
+            $page = FbPage::where('page_id', '=', $request->entry[0]['id'])->first();
+            $userPage = UserPage::where('page_id', '=', $page->id)->get();
+            $users = User::whereIn('id', $userPage->pluck('user_id'))->get();
 
-      $log->raw_value = json_encode($users);
-      $log->save();
-
-      $this->sendNotification($users->pluck('firebase_token'), 'Alert', 'Facebook Notification Received');
-      return response()->json();
-    } catch (Exception $e) {
-      $log->raw_value = json_encode($e->getMessage());
-      $this->er500($e->getMessage());
+            $log->raw_value = json_encode($users);
+            $log->save();
+            // $this->sendNotification($users->pluck('firebase_token'), 'Alert', 'Facebook Notification Received');
+            return response()->json();
+        } catch (Exception $e) {
+            $log->raw_value = json_encode($e->getMessage());
+            $this->er500($e->getMessage());
+        }
+        $log->save();
     }
-      $log->save();
-  }
 
-  public function verifyWebhook(Request $request) {
-    try {
-      $log = new FacebookNotificationLog;
-      $log->raw_value = json_encode($request->all());
-      $log->save();
-
-      $mode  = $request->get('hub_mode');
-      $token = $request->get('hub_verify_token');
-
-      if ($mode === "subscribe" && $this->token and $token === $this->token) {
-        return response($request->get('hub_challenge'));
-      }
-
-      return response("Invalid token!", 400);
-
-    } catch (Exception $e) {
-      $this->er500($e->getMessage());
+    public function sendNoti() {
+        $this->noti(1);
     }
-  }
 
-  public function uploadAudio(Request $request) {
-    $this->validate($request, [
-      'file' => 'required|max:50000'
-    ]);
-
-    $name = 'ac_' . uniqid() . '.wav';
-    $path = 'files' . DIRECTORY_SEPARATOR;
-    $request->file('file')->move($path, $name);
-    $file_name = $path . $name;
-
-    return response()->json(['file_name' => $file_name]);
-  }
-
-  public function deleteAudio(Request $request) {
-    $this->validate($request, [
-      'file_path' => 'required'
-    ]);
-
-    $file = public_path($request->file_path);
-
-    try {
-      File::delete($file);
-      return response()->json(['success' => true]);
-    } catch(Exception $e) {
-      $this->er500($e->getMessage());
+    public function noti($page_id) {
+        try {
+            $response = new StreamedResponse(function () use ($page_id) {
+                while (true) {
+                    $log = FacebookNotificationLog::where('raw_value', 'like', $page_id)->orderBy('created_at', 'desc')->first();
+                    $data = $log === null ? $page_id : $log->raw_value;
+                    echo 'data: ' . json_encode($data) . "\n\n";
+                    ob_flush();
+                    flush();
+                    usleep(5000000);
+                }
+            });
+            $response->headers->set('Content-Type', 'text/event-stream');
+            $response->headers->set('X-Accel-Buffering', 'no');
+            $response->headers->set('Cach-Control', 'no-cache');
+            return $response;
+        } catch (\Exception $e) {
+            return response()->json($e, 500);
+        }
     }
-  }
+
+    public function verifyWebhook(Request $request) {
+        try {
+            $log = new FacebookNotificationLog;
+            $log->raw_value = json_encode($request->all());
+            $log->save();
+
+            $mode  = $request->get('hub_mode');
+            $token = $request->get('hub_verify_token');
+
+            if ($mode === "subscribe" && $this->token and $token === $this->token) {
+                return response($request->get('hub_challenge'));
+            }
+
+            return response("Invalid token!", 400);
+        } catch (Exception $e) {
+            $this->er500($e->getMessage());
+        }
+    }
+
+    public function uploadAudio(Request $request) {
+        $this->validate($request, [
+            'file' => 'required|max:50000'
+        ]);
+
+        $name = 'ac_' . uniqid() . '.wav';
+        $path = 'files' . DIRECTORY_SEPARATOR;
+        $request->file('file')->move($path, $name);
+        $file_name = $path . $name;
+
+        return response()->json(['file_name' => $file_name]);
+    }
+
+    public function deleteAudio(Request $request) {
+        $this->validate($request, [
+            'file_path' => 'required'
+        ]);
+
+        $file = public_path($request->file_path);
+
+        try {
+            File::delete($file);
+            return response()->json(['success' => true]);
+        } catch (Exception $e) {
+            $this->er500($e->getMessage());
+        }
+    }
 }
