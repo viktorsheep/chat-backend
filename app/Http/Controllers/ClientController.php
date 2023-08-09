813cb233d6298ee6156da7d15bf7bc38e32855db
Jupiter <jupitersatt001@protonmailcom>
2023-08-09 19:38:32 +0630

    set client and get conversations

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,25 +5,50 @@ namespace App\Http\Controllers;
 use App\Models\Client;
 use Illuminate\Http\Request;
 use Exception;
+use FFMpeg\Media\Clip;
 use Illuminate\Http\Client\ResponseSequence;
 
 class ClientController extends Controller {
     // set
     public function set(Request $request) {
         try {
-            $client = Client::updateOrCreate([
-                'page_index_id' => $request->page_index_id,
-                'page_id' => $request->page_id,
-                'mid' => $request->mid,
-                'psid' => $request->psid
-            ], [
-                'page_index_id' => $request->page_index_id,
-                'page_id' => $request->page_id,
-                'mid' => $request->mid,
-                'psid' => $request->psid
-            ]);
+            if ($request->conversations) {
+                foreach ($request->conversations as $c) {
+                    $participants = $c['participants']['data'];
 
-            return response()->json($client, 201);
+                    $foundParticipant = collect($participants)->first(function ($participant) use ($request) {
+                        return $participant['id'] !== $request->page_id;
+                    });
+
+                    $psid = $foundParticipant ? $foundParticipant['id'] : null;
+
+                    $client = Client::updateOrCreate([
+                        'page_index_id' => $request->page_index_id,
+                        'page_id' => $request->page_id,
+                        'mid' => $c['id'],
+                        'psid' => $psid
+                    ], [
+                        'page_index_id' => $request->page_index_id,
+                        'page_id' => $request->page_id,
+                        'mid' => $c['id'],
+                        'psid' => $psid
+                    ]);
+                }
+                return response()->json(Client::all(), 201);
+            } else {
+                $client = Client::updateOrCreate([
+                    'page_index_id' => $request->page_index_id,
+                    'page_id' => $request->page_id,
+                    'mid' => $request->mid,
+                    'psid' => $request->psid
+                ], [
+                    'page_index_id' => $request->page_index_id,
+                    'page_id' => $request->page_id,
+                    'mid' => $request->mid,
+                    'psid' => $request->psid
+                ]);
+                return response()->json($client, 201);
+            }
         } catch (Exception $e) {
             return response()->json($e, 500);
         }
